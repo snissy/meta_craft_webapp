172a66599541787b7ff0813e706b11d2d1a233a0
Nils Merkel <nmerkel@kth.se>
2022-01-27 18:46:44 +0100

    this adds a onlinePlayer list object, that show the players that are currently online on the mcServer

---
+++ src/components/topnav/playerLister/onlinePlayerList.js
@@ -0,0 +1,29 @@
+import './onlinePlayerList.css';
+import {useEffect, useState} from "react";
+import mcServerInfoSource from "../../../api/msServerInfo";
+function OnlinePlayerList() {
+
+    const [data, setData] = useState(null);
+    useEffect(()=>{
+        mcServerInfoSource.getServerInfo().then(res=>{setData(res)})
+    },[])
+
+    /**/
+
+    return(<div className={"OnlinePlayerList"}>
+            {data &&
+            <div>
+                <div className={"topRowPlayerList"}>
+                    <p>{"Online: "+ data.minecraftInfo.players.online}</p>
+                </div>
+                <div className={"playerList"}>
+                    {data.minecraftInfo.players.list.map((p, index)=>{
+                        return(<p className={"playerLabel"} key={"k-"+p}>{p}</p>)
+                    })}
+                </div>
+            </div>}
+        </div>
+    )
+
+}
+export default OnlinePlayerList;
--- src/components/topnav/topnav.js
+++ src/components/topnav/topnav.js
@@ -4,12 +4,16 @@ import ZombieHead  from "../../resources/img/Zombie_Head 1.png"
 import PlayerHead  from "../../resources/img/Player_Head 1.png"
 import SkeletonSkull  from "../../resources/img/Skeleton_Skull 1.png"
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import OnlinePlayerList from "./playerLister/onlinePlayerList";
 
 function TopNav() {
     return (
         <div className="TopNav">
             <div className="logoDiv">
                 <img className="metaLogoImg" src={MetacraftLogo} alt={"metacraft logo"}/>
+            </div>
+            <div className="playerListDiv">
+                <OnlinePlayerList/>
             </div>
             <div className="TopNavButtonRow">
                 <button className="TopNavButton" onClick={()=>{console.log("You clicked the Zombie Head")}}>
