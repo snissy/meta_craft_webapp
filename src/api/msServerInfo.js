b6f14b3533bdc619084e645e4c62cd7195ecc6b7
Nils Merkel <nmerkel@kth.se>
2022-01-27 18:45:23 +0100

    this adds a mcServerInfoSource object that can get mcServerInfo

---
+++ src/api/msServerInfo.js
@@ -0,0 +1,17 @@
+import BasicJsonApiCall from "./basicFetcher";
+
+const BASE_NEWS_URL = "/mcServerInfo"
+
+const  mcServerInfoSource={
+
+    apiCall(params) {
+        // The actual fetch
+        return BasicJsonApiCall({baseUrl:BASE_NEWS_URL});
+    },
+
+    getServerInfo(){
+        return mcServerInfoSource.apiCall().then(result=>{return result})
+    }
+}
+
+export default mcServerInfoSource;
